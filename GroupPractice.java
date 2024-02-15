6b63e293c42965da78615b0c83e11955ddaabcd9
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-02-14 19:22:09 -0500

    Added new hello line

--- GroupPractice.java
+++ GroupPractice.java
@@ -3,6 +3,7 @@ package GroupPractice;
 public class GroupPractice {
     public static void main(String[] args) {
 		System.out.println("Hello world!?");
+		System.out.println("Hello World Again!");
 	}
 }
 
